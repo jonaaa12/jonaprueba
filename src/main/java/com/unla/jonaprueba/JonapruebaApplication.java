749c68737641de2b4b8c052f44238ac0441bb452
jonaaa12 <jonathan.a.chaves@gmail.com>
2021-05-16 22:36:41 -0300

    adding spring files

---
+++ src/main/java/com/unla/jonaprueba/JonapruebaApplication.java
@@ -0,0 +1,13 @@
+package com.unla.jonaprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonapruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonapruebaApplication.class, args);
+	}
+
+}
